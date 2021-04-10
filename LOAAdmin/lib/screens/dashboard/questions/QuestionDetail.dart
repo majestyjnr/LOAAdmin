f4f428cc23f22301327bf8cdca2cdf69ecf8e2ba
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-10 15:58:42 -0700

    Commit Updates

---
+++ LOAAdmin/lib/screens/dashboard/questions/QuestionDetail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuestionDetail extends StatefulWidget {
+  QuestionDetail({Key key}) : super(key: key);
+
+  @override
+  _QuestionDetailState createState() => _QuestionDetailState();
+}
+
+class _QuestionDetailState extends State<QuestionDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
