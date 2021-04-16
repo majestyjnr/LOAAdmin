ae4a36b7c077e15be2bc4bc9004f0e2f8c1aacbb
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-16 09:58:58 -0700

    Commit Updates

--- LOAAdmin/lib/screens/dashboard/AddVideo.dart
+++ LOAAdmin/lib/screens/dashboard/AddVideo.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:nuts_activity_indicator/nuts_activity_indicator.dart';
+import 'package:toast/toast.dart';
+import 'package:uuid/uuid.dart';
 import 'package:video_player/video_player.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class AddVideo extends StatefulWidget {
   AddVideo({Key key}) : super(key: key);
@@ -12,8 +17,13 @@ class AddVideo extends StatefulWidget {
 }
 
 class _AddVideoState extends State<AddVideo> {
+  TextEditingController _videoDescription = new TextEditingController();
+
   String dropdownValue = 'Acquisition Section';
   File _video;
+  bool isLoading = false;
+  dynamic _progress;
+  dynamic id = Uuid().v1();
 
   VideoPlayerController _videoPlayerController;
   @override
@@ -54,6 +64,17 @@ class _AddVideoState extends State<AddVideo> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              (_progress == null)
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Uploading ${(_progress * 100).toStringAsFixed(2)}%...',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    )
+                  : Container(),
               Text('Select Video Section:'),
               DropdownButton<String>(
                 isExpanded: true,
@@ -84,6 +105,7 @@ class _AddVideoState extends State<AddVideo> {
               TextField(
                 maxLines: null,
                 minLines: 3,
+                controller: _videoDescription,
                 decoration: InputDecoration(
                   labelText: 'Video Description',
                 ),
@@ -165,11 +187,81 @@ class _AddVideoState extends State<AddVideo> {
               MaterialButton(
                 minWidth: double.infinity,
                 height: 40,
-                child: Text(
-                  'Upload Video',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {},
+                child: isLoading
+                    ? NutsActivityIndicator(
+                        activeColor: Colors.white,
+                      )
+                    : Text(
+                        'Upload Video',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    // Reference a collection to recieve the video
+                    firebase_storage.Reference ref = firebase_storage
+                        .FirebaseStorage.instance
+                        .ref()
+                        .child('videos')
+                        .child(id);
+
+                    // Store Video in the referenced collection
+                    firebase_storage.UploadTask uploadTask =
+                        ref.putFile(_video);
+
+                    // Track Upload Progress
+                    uploadTask.asStream().listen((event) {
+                      setState(() {
+                        _progress = event.bytesTransferred.toDouble() /
+                            event.totalBytes.toDouble();
+                      });
+                    });
+
+                    // GET video URL
+                    var downloadURL =
+                        await uploadTask.whenComplete(() {}).then((value) {
+                      return value.ref.getDownloadURL();
+                    });
+
+                    // Convert downloadURL to a string
+                    final String url = downloadURL.toString();
+
+                    // Save the user details into the database
+                    FirebaseFirestore.instance.collection('Videos Info').add({
+                      'id': id,
+                      'videoSection': dropdownValue,
+                      'videoDescription': _videoDescription.text,
+                      'videoURL': url,
+                    }).then((value) {
+                      _videoDescription.text = '';
+                      setState(() {
+                        isLoading = false;
+                        _video = null;
+                      });
+                      Toast.show(
+                        'Video successfully updated into the database.',
+                        context,
+                        duration: Toast.LENGTH_LONG,
+                        gravity: Toast.TOP,
+                      );
+                    });
+                  } catch (e) {
+                    _videoDescription.text = '';
+                    _video = null;
+
+                    setState(() {
+                      isLoading = false;
+                    });
+                    Toast.show(
+                      'Error uploading video to database.',
+                      context,
+                      duration: Toast.LENGTH_LONG,
+                      gravity: Toast.TOP,
+                    );
+                  }
+                },
                 elevation: 0.5,
                 color: Colors.blue,
               ),
