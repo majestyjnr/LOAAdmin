a3c2ce505531b3d1ac41fb748772f6d2910005b6
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-09-23 00:55:35 -0700

    Commit Updates

--- LOAAdmin/lib/screens/dashboard/ChatRoomsList.dart
+++ LOAAdmin/lib/screens/dashboard/ChatRoomsList.dart
@@ -1,4 +1,8 @@
+import 'package:LOAAdmin/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nuts_activity_indicator/nuts_activity_indicator.dart';
 
 class ChatRoomsList extends StatefulWidget {
   ChatRoomsList({Key key}) : super(key: key);
@@ -8,10 +12,82 @@ class ChatRoomsList extends StatefulWidget {
 }
 
 class _ChatRoomsListState extends State<ChatRoomsList> {
+  Stream chatRoomsStream;
+  String admin = 'admin';
+
+  Widget chatRoomListTile() {
+    return Row(
+      children: [
+        Icon(
+          Icons.account_circle,
+          color: Colors.black,
+          size: 37,
+        ),
+        SizedBox(width: 25)
+
+      ],
+    );
+  }
+
+  Widget chatRooms() {
+    return StreamBuilder(
+      stream: chatRoomsStream,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                padding: EdgeInsets.only(bottom: 40, top: 16),
+                itemCount: snapshot.data.docs.length,
+                reverse: true,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.docs[index];
+                  return Text(ds.id.replaceAll(admin, "").replaceAll("_", ""));
+                },
+              )
+            : Center(
+                child: NutsActivityIndicator(
+                  activeColor: Colors.blue,
+                  radius: 30,
+                ),
+              );
+      },
+    );
+  }
+
+  getChatRooms() async {
+    chatRoomsStream = await DatabaseModels().getChatRoomsList();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getChatRooms();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            CupertinoIcons.back,
+            color: Colors.blue,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
     );
   }
 }
--- LOAAdmin/lib/services/database.dart
+++ LOAAdmin/lib/services/database.dart
@@ -1,3 +1,17 @@
-class DatabaseModels{
-  
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseModels {
+  Future<Stream<QuerySnapshot>> getChatRoomsList() async {
+    return FirebaseFirestore.instance
+        .collection("chatRooms")
+        .orderBy("lastMessageTimeStamp", descending: true)
+        .snapshots();
+  }
+
+  getStudentInfo(String studentEmail) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .where('email', isEqualTo: studentEmail)
+        .get();
+  }
 }
